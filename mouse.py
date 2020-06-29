3607f4ca22181fafd4300dc6e941c34721c4a344
Michal Beneš <20gbenes@dgkralupy.eu>
2020-06-29 15:29:40 +0200

    first commit

---
+++ mouse.py
@@ -0,0 +1,14 @@
+import pyautogui
+# pyautogui.moveTo(220, 1060)
+# pyautogui.click()
+# pyautogui.moveTo(370, 1060)
+# for i in range(100):
+    # pyautogui.moveTo(220, 1060)
+    # pyautogui.click()
+    # pyautogui.moveTo(370, 1060)
+    # pyautogui.click()
+while True:
+    # pyautogui.moveRel(0, 1)  # move mouse 10 pixels down
+    print(pyautogui.position())
+# pyautogui.dragTo(100, 150)
+# pyautogui.dragRel(0, 10)  # drag mouse 10 pixels down
